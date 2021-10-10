ee2dae0aeaa5c0d48beafd755120450b041b9bf2
rahul <rr200636@gmail.com>
2021-10-11 00:04:47 +0530

    rename file from hadcoin to testcoin

--- Testcoin/Nodes/node5001.py
+++ Testcoin/Nodes/node5001.py
@@ -101,7 +101,7 @@ class Blockchain:
 # Creating a Web App
 app = Flask(__name__)
 
-# Creating an address for the node on Port 5000
+# Creating an address for the node on Port 5001
 node_address = str(uuid4()).replace('-', '')
 
 # Creating a Blockchain
@@ -114,7 +114,7 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
-    blockchain.add_transaction(sender = node_address, receiver = 'Hadelin', amount = 1)
+    blockchain.add_transaction(sender = node_address, receiver = 'Rahul', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
@@ -163,7 +163,7 @@ def connect_node():
         return "No node", 400
     for node in nodes:
         blockchain.add_node(node)
-    response = {'message': 'All the nodes are now connected. The Hadcoin Blockchain now contains the following nodes:',
+    response = {'message': 'All the nodes are now connected. The testcoin Blockchain now contains the following nodes:',
                 'total_nodes': list(blockchain.nodes)}
     return jsonify(response), 201
 
@@ -180,4 +180,4 @@ def replace_chain():
     return jsonify(response), 200
 
 # Running the app
-app.run(host = '0.0.0.0', port = 5000)
+app.run(host = '0.0.0.0', port = 5001)
--- Testcoin/Nodes/node5003.py
+++ Testcoin/Nodes/node5003.py
@@ -16,15 +16,14 @@ from urllib.parse import urlparse
 
 # Part 1 - Building a Blockchain
 
-
 class Blockchain:
 
     def __init__(self):
         self.chain = []
         self.transactions = []
-        self.create_block(proof=1, previous_hash='0')
+        self.create_block(proof = 1, previous_hash = '0')
         self.nodes = set()
-
+    
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
@@ -42,18 +41,17 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            hash_operation = hashlib.sha256(
-                str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
                 new_proof += 1
         return new_proof
-
+    
     def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys=True).encode()
+        encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-
+    
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
@@ -63,31 +61,30 @@ class Blockchain:
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
-            hash_operation = hashlib.sha256(
-                str(proof**2 - previous_proof**2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
         return True
-
+    
     def add_transaction(self, sender, receiver, amount):
         self.transactions.append({'sender': sender,
                                   'receiver': receiver,
                                   'amount': amount})
         previous_block = self.get_previous_block()
         return previous_block['index'] + 1
-
+    
     def add_node(self, address):
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
-
+    
     def replace_chain(self):
         network = self.nodes
         longest_chain = None
         max_length = len(self.chain)
         for node in network:
-            response = requests.get(f"http://{node}/get_chain")
+            response = requests.get(f'http://{node}/get_chain')
             if response.status_code == 200:
                 length = response.json()['length']
                 chain = response.json()['chain']
@@ -101,7 +98,6 @@ class Blockchain:
 
 # Part 2 - Mining our Blockchain
 
-
 # Creating a Web App
 app = Flask(__name__)
 
@@ -112,16 +108,13 @@ node_address = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 # Mining a new block
-
-
-@app.route('/mine_block', methods=['GET'])
+@app.route('/mine_block', methods = ['GET'])
 def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
-    blockchain.add_transaction(
-        sender=node_address, receiver='Hadelin', amount=1)
+    blockchain.add_transaction(sender = node_address, receiver = 'You', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
@@ -132,47 +125,37 @@ def mine_block():
     return jsonify(response), 200
 
 # Getting the full Blockchain
-
-
-@app.route('/get_chain', methods=['GET'])
+@app.route('/get_chain', methods = ['GET'])
 def get_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
 # Checking if the Blockchain is valid
-
-
-@app.route('/is_valid', methods=['GET'])
+@app.route('/is_valid', methods = ['GET'])
 def is_valid():
     is_valid = blockchain.is_chain_valid(blockchain.chain)
     if is_valid:
         response = {'message': 'All good. The Blockchain is valid.'}
     else:
-        response = {
-            'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
     return jsonify(response), 200
 
 # Adding a new transaction to the Blockchain
-
-
-@app.route('/add_transaction', methods=['POST'])
+@app.route('/add_transaction', methods = ['POST'])
 def add_transaction():
     json = request.get_json()
     transaction_keys = ['sender', 'receiver', 'amount']
     if not all(key in json for key in transaction_keys):
         return 'Some elements of the transaction are missing', 400
-    index = blockchain.add_transaction(
-        json['sender'], json['receiver'], json['amount'])
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
     response = {'message': f'This transaction will be added to Block {index}'}
     return jsonify(response), 201
 
 # Part 3 - Decentralizing our Blockchain
 
 # Connecting new nodes
-
-
-@app.route('/connect_node', methods=['POST'])
+@app.route('/connect_node', methods = ['POST'])
 def connect_node():
     json = request.get_json()
     nodes = json.get('nodes')
@@ -180,14 +163,12 @@ def connect_node():
         return "No node", 400
     for node in nodes:
         blockchain.add_node(node)
-    response = {'message': 'All the nodes are now connected. The Hadcoin Blockchain now contains the following nodes:',
+    response = {'message': 'All the nodes are now connected. The testcoin Blockchain now contains the following nodes:',
                 'total_nodes': list(blockchain.nodes)}
     return jsonify(response), 201
 
 # Replacing the chain by the longest chain if needed
-
-
-@app.route('/replace_chain', methods=['GET'])
+@app.route('/replace_chain', methods = ['GET'])
 def replace_chain():
     is_chain_replaced = blockchain.replace_chain()
     if is_chain_replaced:
@@ -198,6 +179,5 @@ def replace_chain():
                     'actual_chain': blockchain.chain}
     return jsonify(response), 200
 
-
 # Running the app
-app.run(host='0.0.0.0', port=5002)
+app.run(host = '0.0.0.0', port = 5003)
